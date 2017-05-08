32ecc167a03dc6587f0924fe3ad6a532bfc80532
finnhartshorn <finnhartshorn@gmail.com>
2017-05-09 01:07:29 +1000

    Added a blank search activity. Updated the icon to have a transparent background.

---
+++ app/src/main/java/finnhartshorn/monashlibrary/Books/BookSearchActivity.java
@@ -0,0 +1,15 @@
+package finnhartshorn.monashlibrary.Books;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import finnhartshorn.monashlibrary.R;
+
+public class BookSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_search);
+    }
+}
